d3ef77d870c2577424ec8c08555ab2a752b057b8
LemonFan-maker <2254650260@qq.com>
2024-07-15 15:02:32 +0800

    Complete

---
+++ login02.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+import os
+import sys
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-blink-features')
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(options=chrome_options)
+
+Email02 = os.environ.get('Email02')
+Pass02 = os.environ.get('Pass02')
+
+driver.get("https://ikuuu.pw/auth/login")
+sleep(5)
+driver.find_element(By.XPATH, "/html/body/div[1]/section/div/div/div/div[2]/form/div/div[1]/input").send_keys(str(Email02))
+sleep(1)
+driver.find_element(By.XPATH, "/html/body/div[1]/section/div/div/div/div[2]/form/div/div[2]/input").send_keys(str(Pass02))
+sleep(1)
+driver.find_element(By.XPATH, "/html/body/div[1]/section/div/div/div/div[2]/form/div/div[5]/button").click()
+sleep(5)
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[3]/button[1]").click()
+sleep(1)
+driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/section/div[1]/div/div/a").click()
+sleep(5)
+driver.close()
+
